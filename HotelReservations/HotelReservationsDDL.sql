14f71ab888314d854a3db127020430d416218a43
jstuart15 <stuart.jeffrey@gmail.com>
2017-10-15 11:22:47 -0500

    database summative

--- HotelReservations/HotelReservationsDDL.sql
+++ HotelReservations/HotelReservationsDDL.sql
@@ -5,74 +5,130 @@ drop database if exists HotelReservations;
 create database if not exists HotelReservations;
 use HotelReservations;
 
--- Location Table
-create table if not exists Location(
+-- Locations Table
+create table if not exists Locations(
 	LocationId int not null primary key auto_increment,
     Name varchar(45) not null,
     StreetAddr varchar(45) not null,
     City varchar(45) not null,
-    State varchar(45) not null,
+    State char(2) not null,
     Zip char(10) not null
 );
-insert into Location (Name, StreetAddr, City, State, Zip) 
-	values 	('Stratosphere', '123 Main Street', 'Las Vegas', 'NV', '85560'),
-			('The Grand', '345 Main Street', 'Atlantic City', 'NJ', '11034');
 
--- Amenity Table
-create table if not exists Amenity(
+-- Amenities Table
+create table if not exists Amenities(
 	AmenityId int not null primary key auto_increment,
     Name varchar(45) not null
 );
-insert into Amenity (Name) values ('Casino'), ('Refrigerator'), ('Mini-Bar'), ('Air conditioning'), ('Jacuzzi'),('Master Suite');
 
--- Room Type Table
-create table if not exists RoomType(
+-- RoomTypes Table
+create table if not exists RoomTypes(
 	RoomTypeId int not null primary key auto_increment,
-    Name varchar(45) not null,
+    Description varchar(45) not null,
     BaseRate decimal not null
 );
-insert into RoomType (Name, BaseRate) values ('Standard', 32), ('Premier', 36), ('Select', 44), ('Elite', 56);
 
--- Room Table
-create table if not exists Room(
+-- Rooms Table
+create table if not exists Rooms(
 	RoomId int not null primary key auto_increment,
     RoomTypeId int not null,
-	foreign key (RoomTypeId) references RoomType(RoomTypeId),
     LocationId int not null,
-    foreign key (LocationId) references Location(LocationId),
     RoomNumber int not null,
     Floor varchar(30) not null,
-    OccupancyLimit int not null
+    OccupancyLimit int not null,
+	foreign key (RoomTypeId) references RoomTypes(RoomTypeId),
+    foreign key (LocationId) references Locations(LocationId)
 );
-insert into Room (RoomTypeId, LocationId, RoomNumber, Floor, OccupancyLimit) values (1, 1, 1, 1, 4), (1, 1, 1, 2, 4), (1, 1, 1, 3, 4), (2, 1, 1, 4, 4), (2, 1, 2, 4, 4), (2, 1, 3, 4, 4);
 
 -- RoomAmenity Table
-create table if not exists RoomAmenity(
-	RoomId int not null,
-    foreign key (RoomId) references Room(RoomId),
+create table if not exists RoomAmenities(
+	RoomId int not null auto_increment,
     AmenityId int not null,
-    foreign key (AmenityId) references Amenity(AmenityId),
-    primary key (RoomId, AmenityId)
+    Quantity int not null,
+    primary key (RoomId, AmenityId),
+    foreign key (AmenityId) references Amenities(AmenityId),
+	foreign key (RoomId) references Rooms(RoomId)
 );
-insert into RoomAmenity (RoomId, AmenityId) 
-values 	(1,1), (2,1), (3,1), (4,1), # every room has Casino
-			   (2,2), (3,2), (4,2), # only some rooms have refrigerator
-					  (3,3), (4,3), # only some rooms have Minibar
-		(1,4), (2,4), (3,4), (4,4), # all rooms have AC
-							 (4,5), #only Elite has Jacuzzi
-					  (3,6), (4,6); #only Select/Elite have separate master bedrooms
 
--- customer table
-create table if not exists Customer(
+-- Customers table
+create table if not exists Customers(
 	CustomerId int not null primary key auto_increment,
     FirstName varchar(45) not null,
     LastName varchar(45) not null,
     Phone char(12) null,
     Email varchar(60) not null
 );
-insert into Customer (FirstName, LastName, Phone, Email)
-values ('Corbin', 'March', '', 'cmarch@sg.com'), ('Kyle', 'Rudy', '', 'krudy@sg.com'), ('Eric', 'Wise', '', 'ewise@sg.com');
 
-use HotelReservations;
-select * from Amenity;
-select * from Customer;
+-- ReservationStatuses table
+create table if not exists ReservationStatuses(
+	StatusId int not null primary key auto_increment,
+    StatusName varchar(45) not null
+);
+
+-- Reservations table
+create table if not exists Reservations(
+	ReservationId int not null primary key auto_increment,
+    CustomerId int not null,
+    StatusId int not null default 1,
+    foreign key (StatusId) references ReservationStatuses(StatusId),
+    foreign key (CustomerId) references Customers(CustomerId)
+);
+
+-- InvoiceStatuses table
+create table if not exists InvoiceStatuses(
+	StatusId int not null primary key auto_increment,
+    StatusName varchar(45) not null
+);
+
+-- Guests table
+create table if not exists Guests(
+	GuestId int not null primary key auto_increment,
+    FirstName varchar(45) not null,
+    LastName varchar(45) not null,
+    Age int null
+);
+
+-- Invoices table
+create table if not exists Invoices(
+	InvoiceId int not null primary key auto_increment,
+    CustomerId int not null,
+    Total decimal null default 0,
+    TotalTax decimal null default 0,
+    StatusId int not null,
+    foreign key (CustomerId) references Customers(CustomerId),
+    foreign key (StatusId) references InvoiceStatuses(StatusId)
+);
+
+-- ReservationsRooms table
+create table if not exists ReservationsRooms(
+	ReservationId int not null,
+    RoomId int not null,
+    InvoiceId int not null,
+    StartDate date not null,
+    EndDate date not null,
+    primary key (ReservationId, RoomId, InvoiceId),
+    foreign key (ReservationId) references Reservations(ReservationId),
+    foreign key (RoomId) references Rooms(RoomId),
+    foreign key (InvoiceId) references Invoices(InvoiceId)
+);
+
+-- RoomRates table 
+create table if not exists RoomRates(
+	RateId int not null primary key auto_increment,
+    LocationId int not null,
+    RoomTypeId int not null,
+    StartDate date not null,
+    EndDate date not null,
+    Rate decimal not null,
+    foreign key (LocationId) references Locations(LocationId),
+    foreign key (RoomTypeId) references RoomTypes(RoomTypeId)
+);
+
+-- AddOns table
+create table if not exists AddOns(
+	AddOnId int not null primary key auto_increment,
+    StartDate date not null,
+    EndDate date not null,
+    UnitPrice decimal not null,
+    Description varchar(45) not null,
+);
