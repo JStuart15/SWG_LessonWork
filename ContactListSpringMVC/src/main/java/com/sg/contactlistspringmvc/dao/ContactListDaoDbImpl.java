26fbed945e6bf64407725f2eb401465164417327
jstuart15 <stuart.jeffrey@gmail.com>
2017-10-16 17:43:38 -0500

    contact list daodbImpl

--- ContactListSpringMVC/src/main/java/com/sg/contactlistspringmvc/dao/ContactListDaoDbImpl.java
+++ ContactListSpringMVC/src/main/java/com/sg/contactlistspringmvc/dao/ContactListDaoDbImpl.java
@@ -8,8 +8,10 @@ package com.sg.contactlistspringmvc.dao;
 import com.sg.contactlistspringmvc.model.Contact;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.transaction.annotation.Propagation;
@@ -107,7 +109,39 @@ public class ContactListDaoDbImpl implements ContactListDao {
 
     @Override
     public List<Contact> searchContacts(Map<SearchTerm, String> criteria) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (criteria.isEmpty()){
+            return getAllContacts();
+        } else{
+            // build a prepared statement based on the user's search terms
+            StringBuilder sQuery = new StringBuilder("select * from contacts where ");
+            //build the where clause
+            int numParams = criteria.size();
+            int paramPosition = 0;
+            // we'll put the positional parameters into an array, the 
+            // order of the parameters will match the order in which we 
+            // get the search criteria from the map
+            String[] paramVals = new String[numParams];
+            Set<SearchTerm> keySet = criteria.keySet();
+            Iterator<SearchTerm> iter = keySet.iterator();
+            // build up the where clause based on the key/value pairs in 
+            // the map build where clause and positional parameter array   
+            while(iter.hasNext()){
+                SearchTerm currentKey = iter.next();
+                //if we are not the first one in, we must add an AND to the where clause
+                if (paramPosition > 0){
+                    sQuery.append(" and ");
+                }
+                //now append our criteria name
+                sQuery.append(currentKey);
+                sQuery.append(" = ? ");
+                //grab the value for this search criteria and put it into the paramVals array
+                paramVals[paramPosition] = criteria.get(currentKey);
+                paramPosition++;
+            }
+            return jdbcTemplate.query(sQuery.toString(),
+                    new ContactMapper(),
+                    paramVals);
+        }
     }
     
     private static final class ContactMapper implements RowMapper<Contact>{
