0d03d478dc3248fa930c07e847664ad8e34a224b
jstuart15 <stuart.jeffrey@gmail.com>
2017-12-11 19:12:14 -0600

    security

---
+++ SpringIO/SecuringWeb/src/main/java/com/sg/securingweb/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.securingweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author jstuart15
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+}
